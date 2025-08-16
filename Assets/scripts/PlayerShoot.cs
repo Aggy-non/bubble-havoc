b915c572a81fa2c6971ffdaef52a43f4e20e5e9f
Aggy-non <aggyviswasam@gmail.com>
2025-08-16 17:55:57 +0530

    shooting(perfect?)

--- Assets/scripts/PlayerShoot.cs
+++ Assets/scripts/PlayerShoot.cs
@@ -19,9 +19,12 @@ public class PlayerShoot : MonoBehaviour
 
     void Shoot()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector3 shootDirection = (mousePosition - transform.position).normalized;
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector2 mousePosition2D = new Vector2(mousePosition3D.x, mousePosition3D.y);
+        Vector2 shootDirection = (mousePosition2D - (Vector2)transform.position).normalized;
+        float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, rotation);
         bullet.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(shootDirection.x, shootDirection.y) * bulletSpeed;
         Destroy(bullet, bulletDistance);
     }
